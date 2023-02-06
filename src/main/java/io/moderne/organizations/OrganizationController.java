9aa1697d55737776bede492f9fbb8d9632a37e2e
Scott Jungling <scott@moderne.io>
2023-02-06 09:10:49 -0800

    chore: prometheus scrape format changes (#5)
    clean-up unused dependencies

---
+++ src/main/java/io/moderne/organizations/OrganizationController.java
@@ -0,0 +1,4 @@
+package io.moderne.organizations;
+
+public interface OrganizationController {
+}
---
+++ src/main/java/io/moderne/organizations/OrganizationMetricController.java
@@ -0,0 +1,16 @@
+package io.moderne.organizations;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.exporter.common.TextFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public record OrganizationMetricController(PrometheusMeterRegistry meterRegistry){
+    @GetMapping("/metrics/prometheus")
+    Mono<String> scrape() {
+        return Mono.just(meterRegistry.scrape(TextFormat.CONTENT_TYPE_OPENMETRICS_100));
+    }
+}
+
