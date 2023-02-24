62a08baa9f0dfa74b367ef385ef1f98cca375ae8
Jonathan Schneider <jkschneider@gmail.com>
2023-02-24 09:57:16 -0500

    Filter moderne organization by moderne team membership

--- src/main/java/io/moderne/organizations/OrganizationDataFetcher.java
+++ src/main/java/io/moderne/organizations/OrganizationDataFetcher.java
@@ -9,6 +9,7 @@ import io.moderne.organizations.types.CommitOption;
 import io.moderne.organizations.types.Organization;
 import io.moderne.organizations.types.RepositoryInput;
 import io.moderne.organizations.types.User;
+import org.openrewrite.internal.StringUtils;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
@@ -39,9 +40,15 @@ public class OrganizationDataFetcher {
 
     @DgsQuery
     Flux<Organization> userOrganizations(@InputArgument User user, @InputArgument OffsetDateTime at) {
+        List<String> moderneTeam = List.of(StringUtils.readFully(getClass().getResourceAsStream("/moderne-team.txt"))
+                .split("\n"));
+
         // everybody belongs to every organization, and the "default" organization is listed
         // first in the json that this list is based on, so it will be selected by default in the UI
         return Flux.fromIterable(ownership)
+                // only moderne team members need to see the moderne organization
+                .filter(org -> !org.name().equalsIgnoreCase("moderne") ||
+                               moderneTeam.contains(user.getEmail()))
                 .map(OrganizationDataFetcher::mapOrganization);
     }
 
