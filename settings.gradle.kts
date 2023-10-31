a20ef27aa860da9eeb9337b35fdcf53b6147dfec
Peter Streef <p.streef@gmail.com>
2023-10-31 17:50:30 +0100

    Migrate to `moderne-build-logic`

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "moderne-organizations"
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-    }
-}
