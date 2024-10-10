4d8dd0d3ccd0d1a723e8650624fe5a9dccb3fad7
DovOps <dov.katz@morganstanley.com>
2024-10-10 11:58:56 +0000

    Fix loading of overridden CSV path and create test to confirm this works

---
+++ src/test/java/io/moderne/organizations/OrganizationStructureServiceCustomTest.java
@@ -0,0 +1,35 @@
+package io.moderne.organizations;
+
+import io.moderne.organizations.types.RepositoryInput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureObservability
+@SpringBootTest(properties = {
+        "spring.config.location=classpath:/application-test.yaml"
+})
+class OrganizationStructureServiceCustomTest {
+
+    @Autowired
+    OrganizationStructureService structureService;
+
+    @Test
+    void canReadAllOrganizations() {
+        structureService.readOrganizationStructure();
+    }
+
+    @Test
+    void removeScmFromBitbucketCloneUrl() {
+        OrganizationRepositories organizationRepositories = structureService.readOrganizationStructure().findOrganization("Bitbucket TEST ORG");
+        assertThat(organizationRepositories.repositories())
+                .extracting(RepositoryInput::getOrigin)
+                .containsExactly("bitbucket.example.com/stash");
+        assertThat(organizationRepositories.repositories())
+                .extracting(RepositoryInput::getPath)
+                .containsExactly("openrewrite/rewrite");
+    }
+}
